84630b8e1485ea5841aeb7f9206183f425fcc8c3
Kishor Mamindlapally <95141462+Kishor0100@users.noreply.github.com>
2023-06-19 11:54:53 +0530

    Update Todo.java

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -4,17 +4,15 @@ package com.example.todo.model;
 public class Todo{
     private int id;
     private String todo;
-    private String status;
     private String priority;
+    private String status;
     
-    public Todo(int id, String todo, String status, String priority)
+    public Todo(int id, String todo, String priority, String status)
     {
         this.id = id;
         this.todo = todo;
-        this.status = status;
         this.priority = priority;
-        
-
+        this.status = status;
     }
 
     public int getId()
@@ -34,14 +32,6 @@ public class Todo{
     {
         this.todo = todo;
     }
-    public String getStatus()
-    {
-        return status;
-    }
-    public void setStatus(String status)
-    {
-        this.status = status;
-    }
 
     public String getPriority()
     {
@@ -50,6 +40,15 @@ public class Todo{
     public void setPriority(String priority)
     {
         this.priority = priority;
+    }
+    
+    public String getStatus()
+    {
+        return status;
+    }
+    public void setStatus(String status)
+    {
+        this.status = status;
     }
     
 }
